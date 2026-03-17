97c097249e3d637e013e0f22c1b3d90dff54b923
Jl Sut Pi <sutpi070@gmail.com>
2026-03-17 15:16:32 +0700

    refactor: extract JobListing component and move map logic from JobListings

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import jobs from "../jobs.json";
+import JobListing from "./JobListing";
 
 const JobListings = () => {
   console.log(jobs);
@@ -12,39 +13,7 @@ const JobListings = () => {
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
             {jobs.map((job) => {
-              const { id, type, title, description, salary, location } = job;
-              return (
-                <div
-                  key={id}
-                  className="bg-white rounded-xl shadow-md relative"
-                >
-                  <div className="p-4">
-                    <div className="mb-6">
-                      <div className="text-gray-600 my-2">{type}</div>
-                      <h3 className="text-xl font-bold">{title}</h3>
-                    </div>
-
-                    <div className="mb-5">{description}</div>
-
-                    <h3 className="text-indigo-500 mb-2">{salary} / Year</h3>
-
-                    <div className="border border-gray-100 mb-5"></div>
-
-                    <div className="flex flex-col lg:flex-row justify-between mb-4">
-                      <div className="text-orange-700 mb-3">
-                        {/* <i className="fa-solid fa-location-dot text-lg"></i> */}
-                        {location}
-                      </div>
-                      <a
-                        href="job.html"
-                        className="h-9 bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
-                      >
-                        Read More
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              );
+              return <JobListing job={job} key={job.id} />;
             })}
           </div>
         </div>
