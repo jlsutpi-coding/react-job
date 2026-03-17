eb38317d334dd78c9bf5b088e4f256bfd09a085d
Jl Sut Pi <sutpi070@gmail.com>
2026-03-17 21:47:07 +0700

    feat: add JobPage and switch to React Router Link

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,14 @@ import {
 } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
+import JobPage from "./pages/JobPage";
 
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
+        <Route path="/jobs" element={<JobPage />} />
       </Route>,
     ),
   );
--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { FaMapMarker } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const JobListing = ({ job }) => {
   const [showFullDescription, setShowFullDescription] = useState(false);
@@ -37,12 +38,12 @@ const JobListing = ({ job }) => {
             <FaMapMarker className=" inline text-lg mr-1 mb-1" />
             {location}
           </div>
-          <a
-            href={`/job/${id}`}
+          <Link
+            to={`/job/${id}`}
             className="h-9 bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
           >
             Read More
-          </a>
+          </Link>
         </div>
       </div>
     </div>
